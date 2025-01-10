fbbd6c715eea0b23da583260381b8175225337df
ionutpetru4046 <ionut4046@yahoo.com>
2025-01-10 21:03:55 +0000

    styling added to the header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import { FaSearch } from 'react-icons/fa';
+import Link from 'next/link';
 
 export default function Header() {
   return (
-    <div>
-      Header
-    </div>
+    <header className='bg-slate-200 shadow-md'>
+      <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
+        <Link href='/' >
+            <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
+              <span className=''>Sahand</span>  
+              <span className=''>Estate</span>  
+            </h1>
+        </Link>
+      </div>
+    </header>
   )
 }
