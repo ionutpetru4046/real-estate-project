173faaef9833c15848bb351df908492100dbd18c
ionutpetru4046 <ionut4046@yahoo.com>
2025-01-10 21:55:54 +0000

    clerk auth added to the project

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import { FaSearch } from 'react-icons/fa';
 import Link from 'next/link';
+import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs';
 
 export default function Header() {
   return (
@@ -32,11 +33,16 @@ export default function Header() {
                About 
             </li>
           </Link>
-          <Link href='/'>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+          <SignedOut>
+          <Link href='/sign-in'>
             <li className='hidden md:inline text-slate-700 hover:underline'>
                Sign In 
             </li>
           </Link>  
+          </SignedOut>
         </ul>
       </div>
     </header>
