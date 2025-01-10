1663b724dc248eb07cb7ad2dd900b92384c7dbee
ionutpetru4046 <ionut4046@yahoo.com>
2025-01-10 21:13:20 +0000

    working in header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,7 +11,34 @@ export default function Header() {
               <span className=''>Estate</span>  
             </h1>
         </Link>
+        <form className="bg-slate-100 p-3 rounded-lg flex items-center">
+            <input 
+            type="text"
+            placeholder='Search'
+            className='bg-transparent focus:outline-none w-24 sm:w-64'
+        />
+        <button>
+           <FaSearch className='text-slate-600' />
+        </button>
+        </form>
+        <ul className='flex gap-4'>
+          <Link href='/'>
+            <li className='hidden sm:inline text-slate-700 hover:underline'>
+               Home 
+            </li>
+          </Link>
+          <Link href='/'>
+            <li className='hidden sm:inline text-slate-700 hover:underline'>
+               About 
+            </li>
+          </Link>
+          <Link href='/'>
+            <li className='hidden sm:inline text-slate-700 hover:underline'>
+               Sign In 
+            </li>
+          </Link>  
+        </ul>
       </div>
     </header>
-  )
+  );
 }
