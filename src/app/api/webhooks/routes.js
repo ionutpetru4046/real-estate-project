7dacff498f6a09a504d462ff2210fe3ca12d19f2
ionutpetru4046 <ionut4046@yahoo.com>
2025-01-11 12:19:54 +0000

    routes added to the project

--- src/app/api/webhooks/routes.js
+++ src/app/api/webhooks/routes.js
@@ -48,8 +48,16 @@ export async function POST(req) {
   // For this guide, log payload to console
   const { id } = evt.data
   const eventType = evt.type
-  console.log(`Received webhook with ID ${id} and event type of ${eventType}`)
-  console.log('Webhook payload:', body)
+  
+  if (evt.type === 'user.created') {
+    console.log('user.created')
+  }
+  if (evt.type === 'user.updated') {
+    console.log('user.updated')
+  }
+  if (evt.type === 'user.deleted') {
+    console.log('user.deleted')
+  }
 
-  return new Response('Webhook received', { status: 200 })
+  return new Response('Webhook received', { status: 200 });
 }
