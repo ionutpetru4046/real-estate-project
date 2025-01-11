33688979770a604a0ac331a91133888c275e4018
ionutpetru4046 <ionut4046@yahoo.com>
2025-01-11 12:14:32 +0000

    clerk auth webhooks added to the project

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import Header from "../components/Header";
 
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
