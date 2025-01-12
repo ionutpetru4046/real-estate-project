5f4fa120d967b9de3665c69c4db3dd3276ac8e91
ionutpetru4046 <ionut4046@yahoo.com>
2025-01-12 12:04:20 +0000

    firebase added to the project

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA6xDZ119bVnSgZot4MDuurL36QMxWq0ak",
+  authDomain: "next-real-estate-project.firebaseapp.com",
+  projectId: "next-real-estate-project",
+  storageBucket: "next-real-estate-project.firebasestorage.app",
+  messagingSenderId: "393183319838",
+  appId: "1:393183319838:web:99d2296c67058e64a81b0f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
